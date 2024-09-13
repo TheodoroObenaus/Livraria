7e3e15ced373b1ff5688859f525363a8566fb416
TheodoroObenaus <theodoro.obenaus@gmail.com>
2024-09-13 14:40:43 -0300

    .

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -2,6 +2,12 @@ from rest_framework.serializers import CharField, ModelSerializer
 
 from core.models import Compra, ItensCompra
 
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+        depth = 1
+
 class CompraSerializer(ModelSerializer):
     status = CharField(source="get_status_display", read_only=True)
     usuario = CharField(source="usuario.email", read_only=True)
@@ -9,9 +15,3 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = "__all__"
-
-class ItensCompraSerializer(ModelSerializer):
-    class Meta:
-        model = ItensCompra
-        fields = ("livro", "quantidade")
-        depth = 1
